c6422f4c0618ccc80444acc71b4b4a65cc69fc68
Imhandegbelo <okogbe@github.com>
2024-02-12 06:48:53 -0800

    fix: tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,6 +17,24 @@ class TestBaseModel(unittest.TestCase):
     """
     Tests the attributes of the base model
     """
+    def test_init(self):
+        """
+        """
+        my_model = BaseModel()
+
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        Tests the save method
+        """
+        my_model = BaseModel()
+
+        ini_updated_at = my_model.updated_at
+        cur_updated_at = my_model.save()
+        self.assertNotEqual(ini_updated_at, cur_updated_at)
 
     def setUp(self):
         """
@@ -26,25 +44,34 @@ class TestBaseModel(unittest.TestCase):
 
     def test_basic(self):
         """
-        Tests basic imputs for the BaseModel class
+        Tests basic inputs for the BaseModel class
         """
         my_model = BaseModel()
         my_model.name = "ALX"
-        my_model.number = 60
-        self.assertEqual([my_model.name, my_model.number],
-                         ["ALX", 60])
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number], 
+                ["ALX", 89])
 
-    def test_datetime(self):
+    def test_to_dict(self):
         """
-        Tests for correct datetime format
         """
-        pass
-    
-    def test_datetime(self):
+        my_model = BaseModel()
+
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+
+    def test_str(self):
         """
-        Tests for correct datetime format
         """
-        pass
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith("[BaseModel]"))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
 
 
 if __name__ == '__main__':

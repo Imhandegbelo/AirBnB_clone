695da647d8ccda679d0444d7ffcc46d6a0f6b96e
Imhandegbelo <okogbe@github.com>
2024-02-12 07:37:22 -0800

    update: added more test suits

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,3 +5,16 @@ Test suits for users
 
 import unittest
 from models.base_model import BaseModel
+
+class TestAmenity(unittest.TestCase):
+    """
+    Test suits for amenities
+    """
+
+    def test_amenity(self):
+        """
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

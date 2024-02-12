6b8d5e1daa26243e2acd945f85c074aedb017ac3
Imhandegbelo <okogbe@github.com>
2024-02-12 07:57:42 -0800

    update: file storage testwq

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """
-Test suits for users
+Test suits for storage engine
 """
 
 import unittest
 from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Test FileStorage
+    """
+
+    def test_file_storage(self):
+        """
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

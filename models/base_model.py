71ecaf4852f8b3801bdfa112d81e9a4d0ede9d1a
Imhandegbelo <okogbe@github.com>
2024-02-18 17:07:32 -0800

    update: added console

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
     """
     The base model from which other class inherits
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -46,7 +46,7 @@ class BaseModel:
 
         return obj_dict
 
-
+"""
 if __name__ == "__main__":
     my_model = BaseModel()
     my_model.name = "My First Model"
@@ -62,3 +62,4 @@ if __name__ == "__main__":
             key,
             type(my_model_json[key]),
             my_model_json[key]))
+"""
